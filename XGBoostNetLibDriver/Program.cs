1f2108947f0633416efb8e5d0be2784aa6ce4de8
R. G. Esteves <rgesteves@gmail.com>
2023-04-26 12:46:43 -0700

    Reading the sample dataset

--- XGBoostNetLibDriver/Program.cs
+++ XGBoostNetLibDriver/Program.cs
@@ -1,4 +1,9 @@
-﻿namespace XGBoostNetLibDriver;
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.Data.Analysis;
+
+namespace XGBoostNetLibDriver;
 
 using XGBoostNetLib;
 
@@ -6,6 +11,32 @@ class Program
 {
     static void Main(string[] args)
     {
+        // for a classification task
+	const string datasetFname = "breast_cancer.csv";
+	var dataDir = Path.Combine(AppContext.BaseDirectory, "data");
+	var dataPath = Path.Combine(dataDir, datasetFname);
+	if (!File.Exists(dataPath)) {
+	  Console.WriteLine($"Can't find necessary filename");
+	  Environment.Exit(1);
+	}
+
+	var df = DataFrame.LoadCsv(dataPath, header: false);
+	// (df as IDataView).Schema
+
+	var sampleCount = df.Rows.Count;
+	var featureCount = df.Columns.Count - 1;
+	
+	List<float> features = new();
+	List<float> labels = new();
+	for (int i = 0; i < df.Rows.Count; i++) {
+	  for (int j = 0; j < df.Columns.Count - 1; j++) {
+	    features.Add((float)(df[i, j]));
+	  }
+    	  labels.Add((float)(df[i, df.Columns.Count - 1]));
+	}
+
+        Console.WriteLine($"Checking that I have {sampleCount} samples: ({labels.Count()}, {features.Count() / featureCount})");
+
         Console.WriteLine($"Dealing with XGBoost version {XGBoostUtils.XgbMajorVersion()}.");
         Console.WriteLine("Done!");
     }
