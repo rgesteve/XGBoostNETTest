5eb24aaa6db63c2b946591f7fb5047911257a208
R. G. Esteves <rgesteves@gmail.com>
2023-05-03 23:07:20 -0700

    Simple function to parse json

--- XGBoostNetLibDriverFS/Program.fs
+++ XGBoostNetLibDriverFS/Program.fs
@@ -1,11 +1,18 @@
 ﻿open System
 open System.IO
+open System.Text.Json
 
 let readFile (fname : string) : string option = 
     if File.Exists fname then
         Some (File.ReadAllText fname)
     else
         None
+
+let parseJson (json : string) : JsonElement option =
+    try
+        Some (JsonDocument.Parse(json).RootElement)
+    with
+        _ -> None
 
 [<EntryPoint>]    // This is the attribute syntax in F#
 let main argv =
