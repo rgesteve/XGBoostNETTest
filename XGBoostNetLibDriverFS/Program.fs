99230530c4c5c05925a1e1b19c560319d5dc3c04
R. G. Esteves <rgesteves@gmail.com>
2023-05-06 21:40:07 -0700

    Beginning cli args processing

--- XGBoostNetLibDriverFS/Program.fs
+++ XGBoostNetLibDriverFS/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System
 open System.IO
+open System.Diagnostics
 open System.Text.Json
 
 let readFile (fname : string) : string option = 
@@ -14,11 +15,23 @@ let parseJson (json : string) : JsonElement option =
     with
         _ -> None
 
+let printUsage () =
+  printfn "In usage message"
+  0
+
+let parseCli args =
+    match args with
+    | "-v" :: _ ->
+      printUsage()
+    | _ -> 0
+
 [<EntryPoint>]    // This is the attribute syntax in F#
 let main argv =
+    argv |> List.ofArray |> parseCli |> ignore
+    let proc = Process.GetCurrentProcess()
     let fcontent = readFile (Path.Combine (Environment.GetEnvironmentVariable("HOME"), ".bashrc"))
     let readChar = match fcontent with
                    | Some x -> x.Length
                    | None -> 0
-    printfn "Hello (%d) from F#" readChar
+    printfn "Hello (%d) from process (%s) in F#" readChar proc.ProcessName
     0
